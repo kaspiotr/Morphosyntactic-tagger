dfceea4c6bd606ef2bf3f1c1af63689d1220a324
kaspiotr <kaspiotr@gmail.com>
2019-03-31 21:58:17 +0200

    Implement creating string and writing it to file

--- morphosyntactic_tagger.py
+++ morphosyntactic_tagger.py
@@ -1,9 +1,14 @@
 import xml.etree.ElementTree as ET
+import json
+import jsonlines
+import io
 from utils.classes import Paragraph, Sentence, Token
 
 xml_file_path = '/home/kaspiotr/Dev/MorphosyntacticTagger/resources/NKJP-PodkorpusMilionowy-1.2/010-2-000000001/ann_morphosyntax.xml'
 xml_file_path2 = '/home/kaspiotr/Dev/MorphosyntacticTagger/resources/NKJP-PodkorpusMilionowy-1.2/030-2-000000009/ann_morphosyntax.xml'
 xml_file_path3 = '/home/kaspiotr/Dev/MorphosyntacticTagger/resources/train-gold.xml'
+jsonlines_file_path = "/home/kaspiotr/Dev/MorphosyntacticTagger/resources/output.jsonl"
+json_file_path = "/home/kaspiotr/Dev/MorphosyntacticTagger/resources/output.json"
 
 paragraphs = []
 
@@ -17,7 +22,7 @@ def parse_xml(file_path):
     for event, element in ET.iterparse(file_path, events=("end",)):
         raw_tag = element.tag
         if "{" in element.tag:
-            raw_tag = element.tag.replace(element.tag[element.tag.find("{"): element.tag.find("}")+1], '')
+            raw_tag = element.tag.replace(element.tag[element.tag.find("{"): element.tag.find("}") + 1], '')
         # print(element.tag[element.tag.find("{"): element.tag.find("}")+1])
         if event == "end" and raw_tag == "p":  # use "p" to parse paragraphs in xml files form NKJP, "chunk" to parse chunks from Poleval 2017
             paragraph = Paragraph(element.tag)
@@ -49,8 +54,11 @@ def parse_xml(file_path):
                                 for grand_x4_child in grand_x3_child:
                                     print("\t\t-", grand_x4_child.attrib)
                                     if grand_x4_child.get('name') == 'interpretation':
-                                        print("\t\t\t-", grand_x4_child[0].text)
-                                        token.add_tag(grand_x4_child[0].text)
+                                        tag_with_base_form = grand_x4_child[0].text.split(":")
+                                        tag_list = tag_with_base_form[1:]
+                                        tag = ":".join(tag_list)
+                                        print("\t\t\t-", tag)
+                                        token.add_tag(tag)
 
                     sentence.add_token(token)
                 paragraph.add_sentence(sentence)
@@ -59,8 +67,28 @@ def parse_xml(file_path):
             element.clear()
 
 
+def write_to_jsonlines_file(file_path, paragraph):
+    fp = io.open(file_path, "a", encoding="utf-8")  # file-like object
+    with jsonlines.Writer(fp) as writer:
+        writer.write(paragraph.create_paragraph_line_string())
+    fp.close()
+
+
+def write_paragraph_str_to_file(file_path, paragraph):
+    fp = open(file_path, "w+")
+    fp.write(paragraph.create_paragraph_line_string())
+    fp.close()
+
+# def write_paragraph_to_json_file(json_file_path, paragraph):
+#     data = json.loads(paragraph.create_paragraph_line_string())
+#     print(data)
+
+
 def main():
     for next_paragraph in parse_xml(xml_file_path):
+        # write_paragraph_to_json_file(json_file_path, next_paragraph)
+        # write_to_jsonlines_file(jsonlines_file_path, next_paragraph)
+        write_paragraph_str_to_file(json_file_path, next_paragraph)
         print("Paragraph: ", next_paragraph.paragraph_tag)
         for sentence in next_paragraph.sentences:
             print("\tSentence: ", sentence.sentence_tag)
--- utils/classes.py
+++ utils/classes.py
@@ -6,6 +6,13 @@ class Paragraph:
     def add_sentence(self, sentence):
         self.sentences.append(sentence)
 
+    def create_paragraph_line_string(self):
+        paragraph_str = r"{["
+        for sentence in self.sentences:
+            paragraph_str += sentence.create_sentence_line_str()
+        paragraph_str += "]}"
+        return paragraph_str
+
     def __iter__(self):
         return self.sentences.__iter__()
 
@@ -17,6 +24,13 @@ class Sentence:
 
     def add_token(self, token):
         self.tokens.append(token)
+
+    def create_sentence_line_str(self):
+        sentence_str = "{["
+        for token in self.tokens:
+            sentence_str += token.create_token_line_str()
+        sentence_str += "]}"
+        return sentence_str
 
     def text(self):
         return ''.join(map(lambda token: ' '+token.form if token.space_before else token.form, self.tokens))
@@ -47,6 +61,27 @@ class Token:
 
     def add_proposed_tags(self, proposed_tag):
         self.proposed_tags.append(proposed_tag)
+
+    def create_token_line_str(self):
+        token_str = "{"
+        token_str += "\"changed_form\":\""
+        token_str += self.changed_form
+        token_str += "\","
+        token_str += "\"base_form\":\""
+        token_str += self.base_form
+        token_str += "\","
+        token_str += "\"tag\":\""
+        token_str += self.tag
+        token_str += "\","
+        token_str += "\"proposed_tags\":["
+        for tag in self.proposed_tags:
+            if tag is not self.proposed_tags[0]:
+                token_str += ","
+            token_str += "\""
+            token_str += tag
+            token_str += "\""
+        token_str += "]}"
+        return token_str
 
     def add_interpretation(self, interpretation):
         self.interpretations.append(interpretation)
