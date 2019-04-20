aa1f43e56349c23a0ffc54dbc04eef4ea320cc21
kaspiotr <kaspiotr@gmail.com>
2019-04-20 15:16:54 +0200

    Improve xml namespaces handling

--- morphosyntactic_tagger.py
+++ morphosyntactic_tagger.py
@@ -1,9 +1,9 @@
 import xml.etree.ElementTree as ET
 from utils.classes import Paragraph, Sentence, Token
 
-namespaces = {'cor': 'http://www.tei-c.org/ns/1.0',
-              'nkjp': 'http://www.nkjp.pl/ns/1.0',
-              'xi': 'http://www.w3.org/2001/XInclude'}
+ns = {'cor': '{http://www.tei-c.org/ns/1.0}',
+      'nkjp': '{http://www.nkjp.pl/ns/1.0}',
+      'xi': '{http://www.w3.org/2001/XInclude}'}
 
 xml_file_path = '/home/kaspiotr/Dev/MorphosyntacticTagger/resources/NKJP-PodkorpusMilionowy-1.2/010-2-000000001/ann_morphosyntax.xml'
 
@@ -12,25 +12,25 @@ def parse_xml(file_path):
     global token, sentence, paragraph, interps_base_form
     for event, element in ET.iterparse(file_path, events=("start", "end",)):
         if event == "start":
-            if element.tag == '{http://www.tei-c.org/ns/1.0}p':
+            if element.tag == ns.get('cor') + 'p':
                 paragraph = Paragraph(element.tag)
-            if element.tag == '{http://www.tei-c.org/ns/1.0}s':
+            if element.tag == ns.get('cor') + 's':
                 sentence = Sentence(element.tag)
-            if element.tag == '{http://www.tei-c.org/ns/1.0}f' and element.get('name') == "orth":
+            if element.tag == ns.get('cor') + 'f' and element.get('name') == "orth":
                 token = Token(element.tag)
         if event == "end":
-            if element.tag == '{http://www.tei-c.org/ns/1.0}f':
+            if element.tag == ns.get('cor') + 'f':
                 if element.get('name') == "orth":
                     token.add_changed_form(element[0].text)
                     sentence.add_token(token)
                 if element.get('name') == "nps":
                     token.add_separator(True)
                 if element.get('name') == "interps":
-                    for subelement in list(element.iter('{http://www.tei-c.org/ns/1.0}f')):
+                    for subelement in list(element.iter(ns.get('cor') + 'f')):
                         if subelement.get('name') == "base":
                             interps_base_form = subelement[0].text
                         if subelement.get('name') == "msd":
-                            for proposed_tag_element in list(subelement[0].iter('{http://www.tei-c.org/ns/1.0}symbol')):
+                            for proposed_tag_element in list(subelement[0].iter(ns.get('cor') + 'symbol')):
                                 if proposed_tag_element.get('value') != "":
                                     token.add_proposed_tags(interps_base_form + ":" + proposed_tag_element.get('value'))
                                 else:
@@ -39,9 +39,9 @@ def parse_xml(file_path):
                     disamb_base_form_with_tag = element[0][1][0].text.split(":")
                     token.add_base_form(disamb_base_form_with_tag[0])
                     token.add_tag(":".join(disamb_base_form_with_tag[1:]))
-            if element.tag == '{http://www.tei-c.org/ns/1.0}s':
+            if element.tag == ns.get('cor') + 's':
                 paragraph.add_sentence(sentence)
-            if element.tag == '{http://www.tei-c.org/ns/1.0}p':
+            if element.tag == ns.get('cor') + 'p':
                 yield paragraph
                 element.clear()
 
