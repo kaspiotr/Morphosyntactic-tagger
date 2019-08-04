4c7a054ea419fdc25d5be2d527970133ce7c029d
kaspiotr <kaspiotr@gmail.com>
2019-08-04 09:26:35 +0200

    Fix dictionary/json format
       * add match field to sentence, not to the list of sentences
       * fix getting changed_form from json in unittests after json format
         change

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -2,7 +2,7 @@ import unittest
 import jsonlines
 
 maca_output_jsonl_file_path = '/home/kaspiotr/Dev/MorphosyntacticTagger/resources/output_maca.jsonl'
-nkjp_output_jsonl_file_path = '/home/kaspiotr/Dev/MorphosyntacticTagger/resources/output_nkjp.jsonl'
+nkjp_output_jsonl_file_path = '/home/kaspiotr/Dev/MorphosyntacticTagger/resources/TEST_output_nkjp.jsonl'
 
 
 class TestPreprocessing(unittest.TestCase):
@@ -25,7 +25,7 @@ class TestPreprocessing(unittest.TestCase):
         with jsonlines.open(maca_output_jsonl_file_path) as maca_reader:
             with jsonlines.open(nkjp_output_jsonl_file_path) as nkjp_reader:
                 for json in zip(maca_reader, nkjp_reader):
-                    self.assertMultiLineEqual(json[0]['sentence'][0]['token'][0]['changed_form'], json[1]['sentence'][0]['token'][0]['changed_form'])
+                    self.assertMultiLineEqual(json[0]['sentences'][0]['sentence'][0]['token']['changed_form'], json[1]['sentences'][0]['sentence'][0]['token']['changed_form'])
 
 
 if '__name__' == '__main__':
