39b56654e05b77cdb0e67cec0c75cee7cf775c0b
kaspiotr <kaspiotr@gmail.com>
2019-08-17 16:25:54 +0200

    Add printing paragraphs with different sentence no. between maca and nkjp

--- preprocessing/maca_and_nkjp_output_merge.py
+++ preprocessing/maca_and_nkjp_output_merge.py
@@ -1,22 +1,46 @@
 import jsonlines
+import os
+import json
 
 maca_output_jsonl_file_path = '/home/kaspiotr/Dev/MorphosyntacticTagger/resources/maca_output.jsonl'
 nkjp_output_jsonl_file_path = '/home/kaspiotr/Dev/MorphosyntacticTagger/resources/nkjp_output.jsonl'
+id2pos_in_nkjp_file = {}
 
 
-def print_lines_of_nkjp_and_maca_jsonl_files_that_have_different_changed_forms_in_first_sentences():
-    line_number = 1
-    with jsonlines.open(maca_output_jsonl_file_path) as maca_reader:
+def populate_dicts_with_id2pos_mapping(id2pos_in_nkjp_file_dict):
+    with open(nkjp_output_jsonl_file_path, mode='rb') as nkjp_fh:
         with jsonlines.open(nkjp_output_jsonl_file_path) as nkjp_reader:
-            print("Differences in changed form:")
-            for json in zip(maca_reader, nkjp_reader):
-                if json[0]['sentences'][0]['sentence'][0]['token']['changed_form'] != json[1]['sentences'][0]['sentence'][0]['token']['changed_form']:
-                    print("Line no. %s: maca: %s, nkjp: %s" % (line_number, json[0]['sentences'][0]['sentence'][0]['token']['changed_form'], json[1]['sentences'][0]['sentence'][0]['token']['changed_form']))
-                line_number += 1
+            for nkjp_json in nkjp_reader:
+                id2pos_in_nkjp_file_dict[nkjp_json['id']] = {"first_byte_pos": nkjp_fh.tell()}
+                nkjp_fh.readline()
+
+
+def print_paragraphs_with_different_sentence_no_between_maca_and_nkjp():
+    with open(nkjp_output_jsonl_file_path, mode='rb') as nkjp_fh:
+        with jsonlines.open(maca_output_jsonl_file_path) as maca_reader:
+            print("Differences between maca and nkjp jsons:")
+            maca_sentences_no = 0
+            maca_paragraph_no = 0
+            nkjp_sentences_no = 0
+            nkjp_paragraph_no = 0
+            for maca_json in maca_reader:
+                nkjp_fh.seek(id2pos_in_nkjp_file[maca_json['id']]['first_byte_pos'], os.SEEK_SET)
+                nkjp_json = json.loads(nkjp_fh.readline().decode('utf-8'))
+                maca_paragraph_no += 1
+                for _ in maca_json['sentences']:
+                    maca_sentences_no += 1
+                nkjp_paragraph_no += 1
+                for _ in nkjp_json['sentences']:
+                    nkjp_sentences_no += 1
+                if maca_sentences_no != nkjp_sentences_no:
+                    print("Paragraph no.: %d, sentences no.: maca: %d, nkjp: %d" % (nkjp_paragraph_no, maca_sentences_no, nkjp_sentences_no))
+                maca_sentences_no = 0
+                nkjp_sentences_no = 0
 
 
 def main():
-    print_lines_of_nkjp_and_maca_jsonl_files_that_have_different_changed_forms_in_first_sentences()
+    populate_dicts_with_id2pos_mapping(id2pos_in_nkjp_file)
+    print_paragraphs_with_different_sentence_no_between_maca_and_nkjp()
 
 
 if __name__ == '__main__':
