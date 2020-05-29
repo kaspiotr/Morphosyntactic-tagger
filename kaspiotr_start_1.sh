838d009c788d4d0044ec451dd215e0745f00f5e5
Piotr Kasprzyk <plgkaspiotr@login02.prometheus.cyfronet.pl>
2020-05-29 14:56:14 +0200

    Add test training script for 1st skf split training

---
+++ kaspiotr_start_1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+## Liczba alokowanych węzłów
+#SBATCH -N 1
+## Liczba zadań per węzeł (domyślnie jest to liczba alokowanych rdzeni na węźle)
+#SBATCH --ntasks-per-node=1
+## Ilość pamięci przypadającej na jeden rdzeń obliczeniowy (domyślnie 5GB na rdzeń)
+#SBATCH --mem=48GB
+## Maksymalny czas trwania zlecenia (format HH:MM:SS)
+#SBATCH --time=72:00:00
+## Nazwa grantu do rozliczenia zużycia zasobów
+#SBATCH -A lemkingpu2
+## Zlecenie zadan odbywa sie poprzez podanie ponizszych dwoch opcji systemu kolejkowego
+#SBATCH --partition=plgrid-gpu-v100
+## --gres=gpu[:count] w przypadku gdy nie ma podanej opcji count, system kolejkowy i tak domyslnie alokuje jedna karte na wezle obliczeniowym
+## Po zleceniu zadania system kolejkowy automatycznie ustawia zmienna srodowiskowa $CUDA_VISIBLE_DEVICES oraz zezwala na dostep do zaalokowanych do kart.
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=kaspiotr@gmail.com
+
+module load plgrid/apps/cuda/10.1
+module load plgrid/tools/python/3.6.5
+
+source /net/scratch/people/plgkaspiotr/morphosyntactic-tagger/prometheus_venv/bin/activate
+
+export LD_LIBRARY_PATH=/net/scratch/people/plgkaspiotr/cuda/lib64:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/net/scratch/people/plgkaspiotr/cuda/lib64:$LIBRARY_PATH
+export CPATH=/net/scratch/people/plgkaspiotr/cuda/include:$CPATH
+
+echo {1..1} | xargs -t -P 10 -n 1 srun -n 1 -N 1 --mem=48GB --gres=gpu:1 --time=72:00:00 python3 training.py
+
