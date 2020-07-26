3b583d39aff3acc0ed713283803aa50f904b81a0
kaspiotr <kaspiotr@gmail.com>
2020-07-26 13:14:40 +0200

    Correct problem with empty tag in proposed tags
       * correct problem with empty tag in proposed tags in nkjp_output.jsonl file
       * for instance issue occurred in first token of first sentence
         of paragraph with id 130-5-000001045-1 (there were missing tag 'adv' in first from two proposed tags for token with changed form 'Obok')
       * additional add example with numeral to examples in predict.py script that test trained model

--- predict.py
+++ predict.py
@@ -6,13 +6,16 @@ def main():
     # load the model you trained
     model = SequenceTagger.load('resources/taggers/example-pos/it-3/best-model.pt')
 
-    # create example sentence
+    # create example sentences
     sentence = Sentence('Mam próbkę analizy morfologicznej .')
+    sentence_2 = Sentence('Zjadłem 7 kanapek .')
 
     # predict tags and print
     model.predict(sentence)
+    model.predict(sentence_2)
 
     print(sentence.to_tagged_string())
+    print(sentence_2.to_tagged_string())
 
 
 if __name__ == '__main__':
