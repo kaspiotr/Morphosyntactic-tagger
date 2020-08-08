1459c88c12f2b321cab694b328e09b14ccd4e078
kaspiotr <kaspiotr@gmail.com>
2020-08-08 20:49:30 +0200

    Add another example of a sentence from NKJP to test trained model

--- predict.py
+++ predict.py
@@ -9,13 +9,20 @@ def main():
     # create example sentences
     sentence = Sentence('Mam próbkę analizy morfologicznej .')
     sentence_2 = Sentence('Zjadłem 7 kanapek .')
+    sentence_3 = Sentence('" 1 . Ewakuację wojsk niemieckich ze wszystkich terytoriów okupowanych; 2 . Rozwiązanie '
+                          'partii nazistowskiej i rozpisanie wolnych wyborów; 3 . Położenie kresu dyktaturze; 4 . '
+                          'Przywrócenie dawnej niemieckiej granicy wschodniej sprzed wojny; 5 . Masową redukcję '
+                          'potencjału wojskowego, aby zapobiec na przyszłość nowej agresji; 6 . Kontrolę zbrojeń; '
+                          '7 . Postawienie pod sąd przywódców nazistowskich i zbrodniarzy wojennych " .')
 
     # predict tags and print
     model.predict(sentence)
     model.predict(sentence_2)
+    model.predict(sentence_3)
 
     print(sentence.to_tagged_string())
     print(sentence_2.to_tagged_string())
+    print(sentence_3.to_tagged_string())
 
 
 if __name__ == '__main__':
