bfc744aad717a56f2a8f4adb6a27b82384c6e6c1
kaspiotr <kaspiotr@gmail.com>
2020-09-06 20:49:08 +0200

    Move regex expressions to variables and add new method documentation

--- nkjp_parsing_and_serialization.py
+++ nkjp_parsing_and_serialization.py
@@ -8,21 +8,38 @@ ns = {'cor': '{http://www.tei-c.org/ns/1.0}',
       'nkjp': '{http://www.nkjp.pl/ns/1.0}',
       'xi': '{http://www.w3.org/2001/XInclude}'}
 
+base_form_part_with_pos_tag_regex = '[a-z0-9A-Z\/]+(:[a-z0-9]+)*$'
+nkjp_pos_allowed_base_forms_regex = r"[a-zA-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ\"\'!?.,;\-\s„”–()&*…—§’/ü\[\]+­“°%é=ôòéëä•ţ@‘ö×·$" \
+                                    r"_{}«»~èàášíúČ#¨˝ý><ÉŢő−č|`řñç^âêōěßû]+(:[a-z0-9]+)*"
+nkjp_base_form_first_emoji_regex = "^:[-]?[DOP\/]:"
+nkjp_base_form_second_emoji_regex = "^[:(][-oP]?[\/()\\|\]:]?"
+nkjp_websites_base_forms_first_regex = "\s*http://"
+nkjp_websites_base_forms_second_regex = "news:"
+
 
 def correct_nkjp_base_form_and_tag_format(base_form_with_tag):
-    if re.fullmatch(r"[a-zA-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ\"\'!?.,;\-\s„”–()&*…—§’/ü\[\]+­“°%é=ôòéëä•ţ@‘ö×·$_{}«»~èàášíúČ#¨˝ý><ÉŢő−č|`řñç^âêōěßû]+(:[a-z0-9]+)*", base_form_with_tag) is None:
+    """
+    Handles proper base_form and tag parsing from ann_morphosyntax.xml NKJP corpora files
+
+    :param base_form_with_tag: str
+        base_form and tag of NKJP corpora token divided with ':'
+    :return: tuple (str, str)
+        A tuple of two strings: (base_form, tag)
+    """
+    if re.fullmatch(nkjp_pos_allowed_base_forms_regex, base_form_with_tag) is None:
         if base_form_with_tag == '::interp':
             return ':', 'interp'
-        if re.findall("^:[-]?[DOP\/]:", base_form_with_tag):
-            tag = ":".join(re.search('[a-z0-9A-Z\/]+(:[a-z0-9]+)*$', base_form_with_tag).group().split(':')[1:])
+        if re.findall(nkjp_base_form_first_emoji_regex, base_form_with_tag):
+            tag = ":".join(re.search(base_form_part_with_pos_tag_regex, base_form_with_tag).group().split(':')[1:])
             base_form = base_form_with_tag[0:len(base_form_with_tag) - len(tag) - 1]
             return base_form, tag
-        if re.findall("^[:(][-oP]?[\/()\\|\]:]?", base_form_with_tag):
-            tag = re.search('[a-z0-9A-Z\/]+(:[a-z0-9]+)*$', base_form_with_tag).group()
+        if re.findall(nkjp_base_form_second_emoji_regex, base_form_with_tag):
+            tag = re.search(base_form_part_with_pos_tag_regex, base_form_with_tag).group()
             base_form = base_form_with_tag[0:len(base_form_with_tag) - len(tag) - 1]
             return base_form, tag
-        if re.findall("\s*http://", base_form_with_tag) or re.findall("news:", base_form_with_tag):
-            tag = ":".join(re.search('[a-z0-9A-Z\/]+(:[a-z0-9]+)*$', base_form_with_tag).group().split(':')[1:])
+        if re.findall(nkjp_websites_base_forms_first_regex, base_form_with_tag) \
+                or re.findall(nkjp_websites_base_forms_second_regex, base_form_with_tag):
+            tag = ":".join(re.search(base_form_part_with_pos_tag_regex, base_form_with_tag).group().split(':')[1:])
             base_form = base_form_with_tag[0:len(base_form_with_tag) - len(tag) - 1]
             return base_form, tag
         if base_form_with_tag.startswith("E:") or base_form_with_tag.startswith("D:"):
