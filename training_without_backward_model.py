00de24d036f60c59cf5f6508ea772bb783d3039a
kaspiotr <kaspiotr@gmail.com>
2020-10-31 12:47:29 +0100

    Save training logs in resources_ex_1 directory instead of resources

--- training_without_backward_model.py
+++ training_without_backward_model.py
@@ -131,7 +131,7 @@ def train(skf_split_no, jsonl_file_path):
     :param jsonl_file_path: file in *.jsonl format with paragraphs in a form of a JSON in each line or absolute path to
     that file
     """
-    log.basicConfig(filename='resources/training_ex_1_' + str(skf_split_no) + '.log',
+    log.basicConfig(filename='resources_ex_1/training_ex_1_' + str(skf_split_no) + '.log',
                     format='%(levelname)s:%(message)s', level=log.INFO)
     log.info(flair.device)
     log.info("Is CUDA available: %s " % torch.cuda.is_available())
