6b68983f3a93fe8bce925ab8598f789a54b40587
kaspiotr <kaspiotr@gmail.com>
2020-11-01 01:04:14 +0100

    Add convert_tsv_to_xml_ex_2.py script

--- training_experiment_2.py
+++ training_experiment_2.py
@@ -104,15 +104,15 @@ def train(skf_split_no, jsonl_file_path):
     Model training is based on stratified 10 fold cross validation split indicated by skf_split_no argument.
     Model and training logs are saved in resources_ex_2/taggers/example-pos directory/it-<skf_split_no> (where
     <skf_split_no> is the number of stratified 10 fold cross validation split number used to train the model).
-    Additionally method logs other training logs files and saves them in folder resources of this project under name
-    training_ex_2_<skf_plit_no>.log
+    Additionally method logs other training logs files and saves them in folder resources_ex_2 of this project under
+    name training_ex_2_<skf_plit_no>.log
 
     :param skf_split_no: stratified 10 fold cross validation split number (from range 1 to 10) used to train the model
 
     :param jsonl_file_path: file in *.jsonl format with paragraphs in a form of a JSON in each line or absolute path to
     that file
     """
-    log.basicConfig(filename='resources/training_ex_2_' + str(skf_split_no) + '.log',
+    log.basicConfig(filename='resources_ex_2/training_ex_2_' + str(skf_split_no) + '.log',
                     format='%(levelname)s:%(message)s', level=log.INFO)
     log.info(flair.device)
     log.info("Is CUDA available: %s " % torch.cuda.is_available())
