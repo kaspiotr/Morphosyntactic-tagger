c7c2e9ba788ef76a7a969a9be91ecb3b3ba2b231
kaspiotr <kaspiotr@gmail.com>
2020-11-20 17:23:29 +0100

    Add script to perform training on PolEval2017 data on Prometheus using NVIDIA K40XL

---
+++ train_on_K40XL_pol_eval.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+module load plgrid/apps/cuda/10.0
+module load plgrid/tools/python/3.6.5
+
+source prometheus_venv/bin/activate
+
+export LD_LIBRARY_PATH=/net/scratch/people/plgkaspiotr/cuda/lib64:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/net/scratch/people/plgkaspiotr/cuda/lib64:$LIBRARY_PATH
+export CPATH=/net/scratch/people/plgkaspiotr/cuda/include:$CPATH
+
+srun -n 1 -N 1 --ntasks-per-node=1 -A lemkingpu2 --partition=plgrid-gpu --gres=gpu:1 --mem-per-cpu=64GB --time=72:00:00 --mail-type=ALL --mail-user=kaspiotr@gmail.com python3 training_on_pol_eval_data.py
+
+
